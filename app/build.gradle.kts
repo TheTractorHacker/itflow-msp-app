4067b9e97c8c166d5333652896045916f6f48c5e
Blake Foley <foleyblake859@gmail.com>
2026-07-11 15:43:32 -0500

    Bump version to 1.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.foleyit.itflow"
         minSdk = 34
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.10.2"
+        versionCode = 16
+        versionName = "1.11.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
